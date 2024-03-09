45c456e06f6e84d55ba192b90b9cb2360c902488
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2024-03-09 23:22:20 +0800

    add event listeners to all of the drum buttons

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("working")
+let numberOfDrumButtons = document.querySelectorAll(".drum").length
+
+for (let i = 0; i < numberOfDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        alert("Hello")
+    });
+}
+
